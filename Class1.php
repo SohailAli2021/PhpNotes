a14a5a4fc2d7d93f12cdf1addbee6d265b2a91dc
SohailAli <sohail.qaazi@gmail.com>
2023-10-27 20:43:04 +0500

    First Commit

---
+++ Class1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+<?php
+// $a = 19;//INT
+// $b=12.40;//Float
+// $c=true;//Boolean
+// $d="Ali";
+// $color=["Black","Blue","Red"];
+// echo $a."<br>".$b."<br>".$c."<br>".$d."<br>".$color[0];
+
+
+// $a =["Black","Blue","Red"];
+// var_dump($a);
+// echo Strlen($a);
+// echo str_word_count($a);
+// echo Strrev($a); 
+// echo strpos("My Name is Ali","is");
+
+// $a = 19;
+
+// echo $a--;
+// echo $a;
+
+
+
+?>
+
+</body>
+</html>
