6c58510b0d3dd27c1fa471642185a31021522c1a
SohailAli <sohail.qaazi@gmail.com>
2023-10-30 20:32:24 +0500

    Operater commit

--- Class1.php
+++ Class1.php
@@ -29,6 +29,177 @@
 
 
 
+// Assignment Operater
+
+
+
+// $var = 5;
+// echo $var;
+
+// $var = 10;
+// $var += 5; // $var is now 15
+
+// $var = 10;
+// $var -= 4; // $var is now 6
+
+
+
+// $var = 5;
+// $var *= 3; // $var is now 15
+
+
+// $var = 15;
+// $var /= 3; // $var is now 5
+
+
+
+
+// $var = 17;
+// $var %= 5; // $var is now 2\
+
+
+
+
+// $var = "Hello";
+// $var .= " World"; // $var is now "Hello World
+
+
+// echo $var;
+
+
+
+
+// Relational Operaters
+
+// $a = 5;
+// $b = 5;
+// if ($a == $b) {
+//     echo "a is equal to b";
+// }
+
+// $a = 5;
+// $b = '5';
+// if ($a === $b) {
+//     echo "a is identical to b";
+// } else {
+//     echo "a is not identical to b";
+// }
+
+
+
+// $a = 5;
+// $b = 7;
+// if ($a != $b) {
+//     echo "a is not equal to b";
+// }
+
+
+
+
+// $a = 5;
+// $b = '5';
+// if ($a !== $b) {
+//     echo "a is not identical to b";
+// } else {
+//     echo "a is identical to b";
+// }
+
+
+
+// $a = 5;
+// $b = 3;
+// if ($a > $b) {
+//     echo "a is greater than b";
+// }
+
+
+// $a = 3;
+// $b = 5;
+// if ($a < $b) {
+//     echo "a is less than b";
+// }
+
+
+
+// $a = 5;
+// $b = 5;
+// if ($a >= $b) {
+//     echo "a is greater than or equal to b";
+// }
+
+
+
+// $a = 3;
+// $b = 5;
+// if ($a <= $b) {
+//     echo "a is less than or equal to b";
+// }
+
+
+
+// Logical Operater
+
+
+
+// $a = true;
+// $b = false;
+// if ($a && $b) {
+//     echo "Both a and b are true";
+// } else {
+//     echo "Either a or b (or both) are false";
+// }
+
+
+// $a = true;
+// $b = false;
+// if ($a || $b) {
+//     echo "Either a or b (or both) are true";
+// } else {
+//     echo "Both a and b are false";
+// }
+
+
+
+// $a = true;
+// if (!$a) {
+//     echo "a is false";
+// } else {
+//     echo "a is true";
+// }
+
+
+
+// $arr = array("Car1","Car2","Car3");
+// print_r($arr);
+
+
+// $name =array("blue","green","Yellow");
+// echo "<pre>";
+// print_r($name);
+// echo "</pre>";
+
+// $name =array("blue","green","Yellow");
+// $name[3]="White";
+// $name[4]="Golden";
+// echo "<pre>";
+// print_r($name);
+// echo "</pre>";
+
+// $age =array(
+//     'Ali'=>'23',
+//     'Khan'=>'22',
+//     'farwa'=>'19'
+// );
+// echo "<pre>";
+// print_r($age);
+// echo "</pre>";
+
+
+// $age =array(
+//     'Ali'=>'23',
+//     'Khan'=>'22',
+//     'farwa'=>'19'
+// );
 ?>
 
 </body>
---
+++ class2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+
+// Assignment Operater
+
+// $a =5;
+// echo $a
+
+// // $a = 4;
+// // $a **= 4;
+
+
+// echo $a
+
+
+
+// Relational Operaters
+// $a = "5";
+// $b = "5";
+// echo $a===$b;
+
+// $a = "5";
+// $b = "5";
+// echo $a!==$b;
+
+
+// $a = 10;
+// $b = 15;
+// echo $a<$b;
+
+
+
+// $a = 10;
+// $b = 9;
+// echo $a>=$b;
+
+// $a = 10;
+// $b = 12;
+// echo $a<=$b
+
+
+
+
+    ?>
+</body>
+</html>
