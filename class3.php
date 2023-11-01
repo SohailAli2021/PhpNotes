36f6a775fbb0e29c19780ee3b90dd8ffdf2d8c44
SohailAli <sohail.qaazi@gmail.com>
2023-11-01 20:47:27 +0500

    class commit

---
+++ class3.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $a = true;
+    // $b= false;
+    // if($a && $b){
+    //     echo "Both a and b are true";
+    // }
+    // else {
+    //          echo "Either a or b (or both) are false";
+    //     }
+
+
+    // $a = true;
+    // $b= false;
+    // if($a || $b){
+    //     echo "Both a and b are true";
+    // }
+    // else {
+    //          echo "Either a or b (or both) are false";
+    //     }
+
+
+// $a = false;
+// if (!$a) {
+//     echo "a is false";
+// } else {
+//     echo "a is true";
+// }
+
+// $a = 10;
+// $b=0;
+// if(!$b){
+//     echo "a is true";
+// }
+// else{
+//     echo "a is False";
+// }
+
+// $a = true;
+// $b = true;
+
+// if($a^$b){
+//         echo "YES";
+//     }
+//     else{
+//         echo "NO";
+//     }
+
+
+// $x=8;
+// if($x==2){
+//     echo "valid";
+// }
+// else{
+//     echo "invalid";
+// }
+
+
+// $x=8;
+// if($x==2)
+//     echo "valid";
+
+// else echo "invalid";
+
+
+// $x=14;
+// if($x>=18){
+//     echo "You can Vote";
+// }
+// elseif ($x>=15){
+//     echo "You can vote after three year";
+// }
+// elseif ($x>=10){
+//     echo "You can vote after Eight year";
+// }
+// else{
+//     echo "invalid";
+// }
+
+
+// $lang = "java";
+
+// switch($lang){
+//     case "C#":
+//         echo "Favourite language is C#";
+//         break;
+//         case "Laravel":
+//             echo "Favourite language is Laravel";
+//             break;
+//             case "ASP.NET":
+//                 echo "Favourite language is ASP.NET";
+//                 break;
+//                 default:
+//                 echo "no language found with such name";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
