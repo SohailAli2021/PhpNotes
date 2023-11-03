914a3ed30adcf08513aec068fe1fb8ab715c5446
SohailAli <sohail.qaazi@gmail.com>
2023-11-03 20:41:03 +0500

    Class three commit

---
+++ Class4.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // $color = array("red","blue","green","White","Black","Yellow");
+    // print_r($color);
+
+    // $name = array('blue','green','yellow');
+    // echo '<pre>';
+    // print_r($name);
+    // echo '</pre>';
+
+
+    // $name = array('blue','green','yellow');
+    // $name [3]='White';
+    // $name [4]='Black';
+    // echo '<pre>';
+    // print_r($name);
+    // echo '</pre>';
+
+
+    // $age=array(
+    //    'Ali'=>'23',
+    //     'Aslam'=>'28',
+    //     'Shahzaib'=>'19'
+    // );
+
+
+    // $age=array(
+    //     'Ali'=>'23',
+    //      'Aslam'=>'28',
+    //      'Shahzaib'=>'19'
+    //  );
+    //  echo '<pre>';
+    //  print_r($age['Ali']);
+    //  echo '</pre>';
+
+
+//     $person= array(
+//         'name'=>'Aslam',
+//         'age'=>'19',
+//         'Address'=>'Karachi'
+        
+//     );
+
+
+// echo '<pre>';
+// print_r($person);
+// echo '</pre>';
+
+
+
+
+
+// $person= array(
+//     'name'=>'Aslam',
+//     'age'=>'19',
+//     'Address'=>array('Karachi','Lahore')
+    
+// );
+
+
+// echo '<pre>';
+// print_r($person['Address'][2]='Faislabad');
+// echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+
+    ?>
+
+</body>
+</html>
